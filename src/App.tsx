c35800ca44224649c346a9e61dbfc33760108cd2
Jesús Jiménez Ballano <jjballano@gmail.com>
2022-02-05 15:55:27 +0100

    styled components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import {Container, Header, Logo, Link} from './App.styles'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <Header>
+        <Logo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <Link
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+      </Header>
+    </Container>
   );
 }
 
